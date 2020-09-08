352a3f0dc7e80ea065f0f8be1322433bbf93c9a3
Гуркина Ольга <olakimo@yandex.ru>
2020-09-08 20:09:29 +0300

    Meta class + on_delete set_null

--- posts/models.py
+++ posts/models.py
@@ -18,4 +18,8 @@ class Post(models.Model):
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
      related_name="posts")
-    group = models.ForeignKey(Group,  on_delete=models.CASCADE, related_name="posts", blank=True, null=True)
+    group = models.ForeignKey(Group,  on_delete=models.SET_NULL, related_name="posts", blank=True, null=True)
+
+    class Meta:
+        ordering = ['-pub_date']
+        
